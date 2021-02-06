e1cb4f0841e893b2cef7934e8059308dc26c4b07
Kala Foods <guhan.delta@gmail.com>
2021-02-06 09:52:26 -0500

    Created a selectField() to extract only the required fields from the return JSON response

--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
+import { selectFields } from '../utils/selectFields';
+
 export const baseURL = 'https://hacker-news.firebaseio.com/v0/';
 export const newStoriesURL = `${baseURL}newstories.json`;
 export const storyURL = `${baseURL}item/`;
 
-export const getStory = async (storyId) => await axios.get(`${storyURL + storyId}.json`).then(({data}) => data);
+export const getStory = async (storyId) => await axios.get(`${storyURL + storyId}.json`)
+                                           .then(({data}) => data && selectFields(data)); // Checking for any null objects
+                                           // selectFields() to extract only the required fields/props from return JSON object
 
 export const getStoryIds = async () => await axios.get(newStoriesURL).then(({data}) => data);
