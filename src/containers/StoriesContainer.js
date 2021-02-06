430167c94cff07929a3cd257cd8592a29174ebee
Kala Foods <guhan.delta@gmail.com>
2021-02-06 16:11:27 -0500

    Added the Infinite scroll functionality for scrolling

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react"
 import { getStoryIds } from '../services/hnApi';
 import { Story } from '../components';
 import { StoriesContainerWrapper, GlobalStyle } from '../styles/StoriesContainerStyles';
+import useInfiniteScroll from '../hooks/useInifiniteScroll';
 
 const StoriesContainer = () => {
+  const { count } = useInfiniteScroll();
   const [storyIds, setStoryIds] = useState([]); // Initializing empty array as the getStoryIds returns an array of ID's
 
   useEffect(() => {
@@ -15,7 +17,7 @@ const StoriesContainer = () => {
       <GlobalStyle />
       <StoriesContainerWrapper data-test-id="stories-container">
         <h1>Hacker News</h1>
-        {storyIds.map((storyId,i) => <Story storyId={storyId} key={storyId} />)}
+        {storyIds.slice(0,count).map((storyId,i) => <Story storyId={storyId} key={storyId} />)}
       </StoriesContainerWrapper>
     </>
   )
---
+++ src/hooks/useInifiniteScroll.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+import { MAX_STORIES, STORY_INCREMENT } from '../constants';
+
+const useInifiniteScroll = () => {
+    const [loading, setLoading] = useState(false);
+    const [count, setCount] = useState(STORY_INCREMENT);
+
+    const handleScroll = () =>{
+        /* An element's scrollTop value is a measurement of the distance from the element's top to its topmost visible content. 
+        When an element's content does not generate a vertical scrollbar, then its scrollTop value is 0. 
+
+        The HTMLElement.offsetHeight read-only property returns the height of an element, including vertical padding and borders, as an int */
+
+        if(window.innerHeight + document.documentElement.scrollTop 
+            !== document.documentElement.offsetHeight || loading){
+            return false;
+        }
+
+        setLoading(true);
+    };
+
+    useEffect(() =>{ 
+        if(!loading) return;
+
+        if(count + STORY_INCREMENT >= MAX_STORIES){
+            setCount(MAX_STORIES); /* If the story is like 480 and the user scrolls down to read more, count + increment = 510 > 500, so
+            stop the scrolling here */
+        }else{
+            setCount(count + STORY_INCREMENT); // Increment the story if the current count and after scroll coun < 500
+        }
+
+        setLoading(false);
+    }, [loading]);
+
+    // Cleanup function to make sure that they don't stay around and be creating event listeners
+    useEffect(()=>{
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    },[]);
+    
+    return { count };
+}
+
+export default useInifiniteScroll
