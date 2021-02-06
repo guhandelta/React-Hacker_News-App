924f6302ad135e01df66d3891b8a2a997fc73008
Kala Foods <guhan.delta@gmail.com>
2021-02-06 02:25:22 -0500

    Successfully fetched the StoryId's

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react"
+import { getStoryIds } from './services/hnApi';
+
 const App = () => {
+  const [storyIds, setStoryIds] = useState([]); // Initializing empty array as the getStoryIds returns an array of ID's
+
+  useEffect(() => {
+    getStoryIds().then(data => setStoryIds(data));
+  }, [])
+
   return (
     <div>
-      <h1>App Works!!.......</h1>
+      <h1>App Still Works!!.......</h1>
+      <h3>{JSON.stringify(storyIds)}</h3>
     </div>
   )
 }
--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const baseURL = 'https://hacker-news.firebaseio.com/v0/';
-export const newStoriesURL = `${baseURL}newStories.json`;
+export const newStoriesURL = `${baseURL}newstories.json`;
 export const storyURL = `${baseURL}item/`;
 
-export const getStoryIds = async () => await axios.get(newStoriesURL).then(data => data);
+export const getStoryIds = async () => await axios.get(newStoriesURL).then(({data}) => data);
